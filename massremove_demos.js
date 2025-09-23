5572febd860d43b13b24d49e5d44c3ef6158e35a
Nixos <48947296+nirgranth@users.noreply.github.com>
2025-09-23 14:57:35 +0530

    1 hr timout

--- massremove_demos.js
+++ massremove_demos.js
@@ -57,7 +57,7 @@ function removeNextPackage(appIds, i) {
     }).then(data => {
         if (data && data.success === 84) {
             console.log(`Rate limit exceeded. Retrying after delay...`);
-            setTimeout(() => removeNextPackage(appIds, i), 600000); // Retry after 10 mins
+            setTimeout(() => removeNextPackage(appIds, i), 3600000); // Retry after 1 hr
         } else {
             console.log(`Removed: ${appIds[i]} (${i + 1}/${appIds.length})`);
             removeNextPackage(appIds, i + 1);
